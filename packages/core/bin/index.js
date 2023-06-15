258f650b325c895703e486c519616347fda7f0d2
man.wang <man.wang@symbio.com>
2023-06-15 22:00:54 +0800

    '增加node版本检查以及core函数改为try/catch捕获全部错误'

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,6 +1,3 @@
 #!/usr/bin/env node
 
 require("../lib")(process.argv.slice(2));   // 执行本包入口文件
-
-
-console.log('脚手架测试')
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,5 +1,7 @@
 'use strict';
+const semver = require('semver');
 const { log } = require('@man-cli-test/utils');
+const LOWEST_NODE_VERSION = '12.0.0';
 
 
 
@@ -9,11 +11,23 @@ function checkPkgVersion() {
   log.info('version:', pkg.version);
 }
 
+function checkNodeVersion() {
+  const currentVersion = process.version, lowestVersion = LOWEST_NODE_VERSION;
+  throw new Error(`当前node版本不得低于v${lowestVersion}`);
+  if (!semver.gte(currentVersion, lowestVersion)) {
+  }
+}
+
 
 module.exports = core;
 
 function core() {
-  checkPkgVersion();
+  try {
+    checkPkgVersion();
+    checkNodeVersion()
+  } catch (error) {
+    log.error(error.message);
+  }
 }
 
 
