275515822f817c198119298038d6279be8c527ec
man.wang <man.wang@symbio.com>
2023-06-15 22:48:11 +0800

    自动降级以及用户主目录检测

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,6 +1,10 @@
 'use strict';
 const semver = require('semver');
+const { homedir } = require('os');
+const pathExists = require('path-exists').sync;
+
 const { log } = require('@man-cli-test/utils');
+
 const LOWEST_NODE_VERSION = '12.0.0';
 
 
@@ -13,8 +17,24 @@ function checkPkgVersion() {
 
 function checkNodeVersion() {
   const currentVersion = process.version, lowestVersion = LOWEST_NODE_VERSION;
-  throw new Error(`当前node版本不得低于v${lowestVersion}`);
   if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(`当前node版本不得低于v${lowestVersion}`);
+  }
+}
+
+
+// process.geteuid()无法在windows中使用，因此这个功能不做演示
+function checkRoot() {
+  // console.log(process.geteuid())
+  // const rootCheck = require('root-check');
+  // rootCheck();
+}
+
+
+function checkUserHome() {
+  // console.log('usr', homedir())    // C:\Users\满
+  if (!homedir() || !pathExists(homedir())) {
+    throw new Error('当前登录用户主目录不存在')
   }
 }
 
@@ -23,8 +43,11 @@ module.exports = core;
 
 function core() {
   try {
-    checkPkgVersion();
+    checkPkgVersion()
     checkNodeVersion()
+    // checkRoot()
+    checkUserHome();
+
   } catch (error) {
     log.error(error.message);
   }
