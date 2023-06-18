075d1479a8b7a136a69c6254b71790e5cfed3562
man.wang <man.wang@symbio.com>
2023-06-18 22:58:33 +0800

    commander初始化及命令注册了解

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -2,6 +2,7 @@
 const semver = require('semver');
 const { homedir } = require('os');
 const pathExists = require('path-exists').sync;
+const { Command } = require('commander')
 
 const { log } = require('@man-cli-test/utils');
 
@@ -10,6 +11,8 @@ const LOWEST_NODE_VERSION = '12.0.0';
 
 
 const pkg = require('../package.json');
+const program = new Command();
+
 
 function checkPkgVersion() {
   log.info('version:', pkg.version);
@@ -39,6 +42,60 @@ function checkUserHome() {
 }
 
 
+function registryCommand() {
+  /* -------------------------官网示例演示选项功能----------------------------------*/
+
+  // program
+  //   .option('-d, --debug', 'output extra debugging')
+  //   .option('-s, --small', 'small pizza size')
+  //   .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+  // program.parse(process.argv);
+
+  // const options = program.opts();
+  // if (options.debug) console.log(options);
+  // console.log('pizza details:');
+  // if (options.small) console.log('- small pizza size');
+  // if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+
+  /* -------------------------官网示例演示命令功能----------------------------------*/
+  // program
+  //   .command('clone <source> [destination]')
+  //   .description('clone a repository into a newly created directory')
+  //   .action((source, destination) => {
+  //     console.log('clone command called');
+  //   });
+
+
+  /* -------------------------官网示例演示命令参数功能----------------------------------*/
+
+  // program
+  //   .version(pkg.version)
+  //   .argument('<username>', 'user to login')
+  //   .argument('[password]', 'password for user, if required', 'no password given')
+  //   .action((username, password) => {
+  //     console.log('username:', username);
+  //     console.log('password:', password);
+  //   });
+
+  /* -------------------------在帮助信息中加上脚手架命令----------------------------------*/
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [option]')        // Usage: man-cli-test <command> [option]
+    .option('-d, --debug', '是否开启调试模式', false)
+    .option('-tp, --targetPath <targetPath>', '是否指定本地调试文件路径', '')
+    .version(pkg.version);
+
+
+
+  program
+    .command('init [projectName]')
+    .option('-f, --force', '是否强制化初始项目')
+
+
+  program.parse(process.argv)   // 此行必需要有
+}
+
 module.exports = core;
 
 function core() {
@@ -47,7 +104,7 @@ function core() {
     checkNodeVersion()
     // checkRoot()
     checkUserHome();
-
+    registryCommand()
   } catch (error) {
     log.error(error.message);
   }
