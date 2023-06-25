a162d614860419bd63ce635c961c10225629285e
man.wang <man.wang@symbio.com>
2023-06-25 16:53:22 +0800

    core去除utils依赖

--- packages/utils/lib/log/index.js
+++ packages/utils/lib/log/index.js
@@ -2,7 +2,7 @@
 
 const log = require('npmlog');
 log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
-log.heading = ' man-cli-dev ';
+log.heading = ' man-cli-test ';
 log.headingStyle = { fg: 'black', bg: 'white' };
 log.addLevel('success', 2000, { 'bg': 'red' });
 
